50ec83eca8ce2e330ba39a2c694cd7ede2613544
KaroHovhannisyan <kar.hovhannisyan.95@gmail.com>
2018-03-18 00:42:43 +0400

    inital

---
+++ server.js
@@ -0,0 +1,12 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+const port = 8000;
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect("mongodb://gameByUs:gameByUs1995@ds215759.mlab.com:15759/game", (err, database) => {
+  if (err) return console.log(err)
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
